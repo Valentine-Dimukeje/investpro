5f07f95f1797d68ea8cee1f246bb69834a316ec5
Valentine-Dimukeje <valentinedimukeje@gmail.com>
2025-12-16 19:08:09 +0100

    updated  settings.py, views.py and urls.py

--- investpro_backend/settings.py
+++ investpro_backend/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
