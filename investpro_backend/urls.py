d6d51f26c086ebcc61a04caef8d6ffa3378659d8
Valentine-Dimukeje <valentinedimukeje@gmail.com>
2025-12-17 21:11:35 +0100

    update  views.py and urls.py

--- investpro_backend/urls.py
+++ investpro_backend/urls.py
@@ -1,40 +1,20 @@
-"""
-URL configuration for investpro_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
-from core.views import create_admin_once
-from core.views import reset_admin_password
 from django.views.generic import TemplateView
-
+from core.views import create_admin_once, reset_admin_password
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("core.urls")),
+
     path("create-admin-once/", create_admin_once),
     path("reset-admin-password/", reset_admin_password),
-     path(
+
+    path(
         "robots.txt",
         TemplateView.as_view(
             template_name="robots.txt",
-            content_type="text/plain"
+            content_type="text/plain",
         ),
-    
-    
+    ),
 ]
-
-
