3dc7d6db3d17b5c6ae6ad8ed37d1a56cd4e16083
Valentine-Dimukeje <valentinedimukeje@gmail.com>
2025-12-18 10:14:10 +0100

    update views.py and settings.py

--- investpro_backend/settings.py
+++ investpro_backend/settings.py
@@ -146,7 +146,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
     ),
-    "UNAUTHENTICATED_USER": None,
+    
 }
 
 SIMPLE_JWT = {
