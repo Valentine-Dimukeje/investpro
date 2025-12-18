732dd91acb645cd022424585ffc452915b5c6a54
Valentine-Dimukeje <valentinedimukeje@gmail.com>
2025-12-18 09:56:12 +0100

    Fix admin save_model placement

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,6 @@
-# core/admin.py
 from django.contrib import admin
 from .models import Profile, Transaction, Referral, Device
 from core.services.wallet import approve_deposit
-
 
 
 @admin.register(Profile)
@@ -10,12 +8,24 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'phone', 'main_wallet', 'profit_wallet')
     search_fields = ('user__username', 'phone')
 
+
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ('user', 'type', 'amount', 'status', 'created_at')
     list_filter = ('type', 'status', 'created_at')
     search_fields = ('user__username',)
     ordering = ('-created_at',)
+
+    def save_model(self, request, obj, form, change):
+        if (
+            change
+            and obj.type == "deposit"
+            and obj.status == "completed"
+        ):
+            approve_deposit(obj)
+
+        super().save_model(request, obj, form, change)
+
 
 @admin.register(Referral)
 class ReferralAdmin(admin.ModelAdmin):
@@ -27,15 +37,3 @@ class ReferralAdmin(admin.ModelAdmin):
 @admin.register(Device)
 class DeviceAdmin(admin.ModelAdmin):
     list_display = ("user", "device_name", "ip_address", "last_active")
-
-def save_model(self, request, obj, form, change):
-    if (
-        change
-        and obj.type == "deposit"
-        and obj.status == "completed"
-    ):
-        approve_deposit(obj)
-    else:
-        super().save_model(request, obj, form, change)
-
-    
